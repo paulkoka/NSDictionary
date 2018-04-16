a84bec62ed8895b330510055843d939fa34c5f5f
paulkoka <pavelkoka@gmail.com>
2018-04-16 14:46:56 +0300

    task moved to method

--- NSDictionary/NSDictionary/ViewController.m
+++ NSDictionary/NSDictionary/ViewController.m
@@ -16,12 +16,16 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-#pragma mark NSDictionary
-    NSAutoreleasePool *pool1 = [[NSAutoreleasePool alloc] init];
+    [self doTask];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+-(void)doTask{
     //        Please, find your tasks on NSDictionary and NSMutableDictionary bellow:
     //
     //        Create NSDictionary using literal declaration.
-    NSDictionary *dictionaryUsingLiteral = @{@"0": @"NullNumber",
+    
+    NSDictionary *dictionaryUsingLiteral = @{@"0": @"Zero",
                                              @"1": @"One",
                                              @"2": @"Two",
                                              @"3": @"Three",
@@ -30,18 +34,22 @@
                                              };
     
     //        Make it mutable and add several new entries.
-    dictionaryUsingLiteral = [dictionaryUsingLiteral mutableCopy];
-    [(NSMutableDictionary *) dictionaryUsingLiteral setObject:@"SixMutable" forKey:@"6"];
-    [(NSMutableDictionary *) dictionaryUsingLiteral setObject:@"SevenMutable" forKey:@"7"];
+    
+    NSMutableDictionary *mutableDictionaryUsingLiteral = [NSMutableDictionary
+                                            dictionaryWithDictionary:dictionaryUsingLiteral];
+    [mutableDictionaryUsingLiteral setObject:@"SixMutable" forKey:@"6"];
+    [mutableDictionaryUsingLiteral setObject:@"SevenMutable" forKey:@"7"];
     
     //        Sort it in an ascending and descending order.
-    //        ascending
-    NSMutableArray *tempArrayForSortingDictionary = [[dictionaryUsingLiteral allKeys]
-                                                     sortedArrayUsingComparator:^NSComparisonResult(id  _Nonnull obj1, id  _Nonnull obj2) {
-                                                         return [obj1 compare:obj2];
-                                                     }];
     
-    void(^printDictionaryByOrderedKeysBlock)(NSArray*,NSDictionary*)=^(NSArray* array,
+    //        ascending order
+    NSArray *tempArrayForSortingDictionary = [[dictionaryUsingLiteral allKeys]
+            sortedArrayUsingComparator:^NSComparisonResult(id  _Nonnull obj1, id  _Nonnull obj2) {
+            return [obj1 compare:obj2];
+            }];
+    
+    
+    void(^printDictionaryByOrderedKeysBlock)(NSArray*,NSDictionary*) = ^(NSArray* array,
                                                                        NSDictionary* dictionary)
     {
         for (NSString *key in array)
@@ -49,16 +57,18 @@
             NSLog(@"%@ = %@", key, [dictionary objectForKey:key]);
         };
     };
+    
     printDictionaryByOrderedKeysBlock(tempArrayForSortingDictionary, dictionaryUsingLiteral);
     
+    //descending order
     printDictionaryByOrderedKeysBlock([[tempArrayForSortingDictionary reverseObjectEnumerator]
                                        allObjects], dictionaryUsingLiteral);
     
     
     //        Check whether required value is contained in the dictionary.
-    __block id testValue =@"blbablabla";
+    __block id testValue = @"blbablabla";
     
-    void(^testIfInTheDictionaryBlock)(void)=^{
+    void(^testIfInTheDictionaryBlock)(void) = ^{
         if ([[dictionaryUsingLiteral allKeysForObject:testValue] firstObject]) {
             NSLog(@"%@ is in the dictionary", testValue);
         } else{
@@ -68,15 +78,9 @@
     
     testIfInTheDictionaryBlock();
     
-    
     testValue = @"Three";
     testIfInTheDictionaryBlock();
-    
-    [dictionaryUsingLiteral release];
-    [pool1 release];
-    // Do any additional setup after loading the view, typically from a nib.
 }
-
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
